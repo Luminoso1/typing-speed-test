ef49374bcdc5875c910f36d9b6f172bcdb7dff32
Luminoso1 <novoaavila16gmail.com>
2025-12-29 18:26:36 -0500

    fix some styles issues

--- src/components/TypeBox.tsx
+++ src/components/TypeBox.tsx
@@ -1,3 +1,14 @@
 export default function TypeBox() {
-  return <div className="type-box">characters</div>
+  return (
+    <div className="type-box">
+      <span className="text-filled">W</span>
+      <span className="text-filled">e</span>
+      <span className="text-filled">l</span>
+      <span className="text-filled">c</span>
+      <span className="text-filled">o</span>
+      <span className="text-error">m</span>
+      <span className="text-filled">e</span>
+      <span className="text-empty cursor">!</span>
+    </div>
+  )
 }
