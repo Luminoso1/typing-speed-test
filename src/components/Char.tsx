ddad07a5dc98b08413b62b9a1aff77ba72ad6037
Luminoso1 <novoaavila16gmail.com>
2026-02-07 12:51:17 -0500

    remove forwardRef

--- src/components/Char.tsx
+++ src/components/Char.tsx
@@ -1,28 +1,29 @@
-import { forwardRef, memo } from 'react'
+import { memo } from 'react'
 import clsx from 'clsx'
 import type { Character } from '../lib/types'
 
-interface CharacterProps extends Character {
+interface Props extends Character {
+  ref?: React.Ref<HTMLSpanElement>
   isCurrent?: boolean
 }
 
-const CharacterItem = memo(
-  forwardRef<HTMLSpanElement, CharacterProps>(function CharacterItem(
-    { value, state, isCurrent },
-    ref,
-  ) {
-    const spanClasses = clsx('relative  transition-colors  duration-300', {
-      'text-neutral-600': state === 'EMPTY',
-      'text-neutral-300/80': state === 'FILLED',
-      'text-error': state === 'ERROR',
-      cursor: isCurrent,
-    })
-    return (
-      <span ref={ref} className={spanClasses}>
-        {value}
-      </span>
-    )
-  }),
-)
+const CharacterItem = memo(function CharacterItem({
+  ref,
+  value,
+  state,
+  isCurrent,
+}: Props) {
+  const spanClasses = clsx('relative  transition-colors  duration-300', {
+    'text-neutral-600': state === 'EMPTY',
+    'text-neutral-300/80': state === 'FILLED',
+    'text-error': state === 'ERROR',
+    cursor: isCurrent,
+  })
+  return (
+    <span ref={ref} className={spanClasses}>
+      {value}
+    </span>
+  )
+})
 
 export default CharacterItem
