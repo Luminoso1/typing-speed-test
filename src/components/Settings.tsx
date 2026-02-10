83e8c2411c52994867e0331346960312def860cd
Luminoso1 <novoaavila16gmail.com>
2026-02-09 22:56:58 -0500

    hide radios & outline when has focus added

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -120,7 +120,7 @@ const Labels = function <T>({
                 'focus cursor-pointer rounded-full px-5 py-3 text-sm capitalize transition-all duration-300',
                 'hover:text-neutral-0 text-neutral-400 hover:bg-neutral-700/60',
                 'has-checked:bg-blue-600 has-checked:text-white',
-                'has-focus-visible:ring-2 has-focus-visible:ring-blue-400',
+                'outline-offset-4 outline-blue-400 has-focus-visible:outline-1',
               )}
             >
               <input
@@ -129,6 +129,7 @@ const Labels = function <T>({
                 name={name}
                 checked={isChecked}
                 onChange={() => onChange(value)}
+                className="sr-only"
               />
               {value as string}
             </label>
